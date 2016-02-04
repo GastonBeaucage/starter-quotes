97607c9a5df867bf26d64bf67eefd39b9bf82d1e
denis <dturitsa@gmail.com>
2016-02-04 12:14:07 -0800

    Last link done

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,25 @@
+<?php
+
+class First extends Application {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+
+	function index()
+	{
+                $this->data['pagebody'] = 'justone';
+            
+                $record = $this->quotes->first();
+                $this->data['who'] = $record['who'];
+                $this->data['mug'] = $record['mug'];
+                $this->data['what'] = $record['what'];
+
+		$this->render();
+
+	}
+
+}
+
